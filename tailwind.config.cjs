0b4c97bb86ee51bf4fd66884b7eb0162e49bbade
notskamr <66314478+notskamr@users.noreply.github.com>
2023-05-30 17:26:54 +0530

    reverse darkness values

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@
 
 const desktopImgUrl = "https://source.unsplash.com/random/1920x1080/?tree";
 const mobileImgUrl = "https://source.unsplash.com/random/1080x1920/?tree";
-const darkenGradient = [0.8, 0.6];
+const darkenGradient = [0.6, 0.8];
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
